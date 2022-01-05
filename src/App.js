bd72cd8c873c4553c99d5004860e6bb32cb1f6d2
Andres9888 <Andres9888@Gmail.com>
2022-01-05 16:11:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react"
 
 const App = () => (
   <Fragment>
-    <h1>Your policies</h1>
+    <h1>Your policies commit change test</h1>
   </Fragment>
-);
+)
 
-export default App;
+export default App

9d5045fce22b59d0ebbec17362e63ecdc99d6bfc
Andres9888 <Andres9888@Gmail.com>
2022-01-06 12:49:58 -0500

    make card dynamic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React from "react"
 import useSWR from "swr"
 import axios from "axios"
 import { Container, Row, Col } from "react-bootstrap"
@@ -10,31 +10,53 @@ const App = () => {
     "https://7946a218-d225-4d0e-80ac-450bbc9713a0.mock.pstmn.io/booking",
     fetcher
   )
+  if (!data) return <div>Loading...</div>
+  if (error) return <div>failed to load</div>
 
-  console.log(data)
+  const { policies } = data
 
   return (
-    <Fragment>
-      <Container fluid>
-        <PageTitle>PROTECTION</PageTitle>
-        <Row>
-          <Col>
-            <InsuranceCard>
-              <InsurancePolicy>
-                <Title>London to Paris</Title>
-                <Description>
-                  XXXX-XXXX-INS | Baggage Cover ; Gadget Cover ; Health Cover
-                </Description>
-              </InsurancePolicy>
-            </InsuranceCard>
-          </Col>
-        </Row>
-      </Container>
-    </Fragment>
+    <StyledContainer fluid>
+      <PageTitle>PROTECTION</PageTitle>
+      {policies.map(
+        ({
+          title,
+          id,
+          description,
+          coverage_start_date,
+          coverage_end_date,
+          partner,
+        }) => {
+          return (
+            <Row>
+              <Col>
+                <InsuranceCard>
+                  <InsurancePolicy>
+                    <Title>{title}</Title>
+                    <Description>{`${id} | ${description}`}</Description>
+                    <Divider />
+                    <CoverageDates>{`${coverage_start_date} to ${coverage_end_date}`}</CoverageDates>
+                    <CoverageDatesTitle>Coverage dates</CoverageDatesTitle>
+                    <img src={partner.logo} alt="logo" />
+                  </InsurancePolicy>
+                </InsuranceCard>
+              </Col>
+            </Row>
+          )
+        }
+      )}
+    </StyledContainer>
   )
 }
 
 export default App
+
+const StyledContainer = styled(Container)`
+  height: 100vh;
+  background-color: #f6f6f6;
+  padding-right: 16px;
+  padding-left: 16px;
+`
 
 const PageTitle = styled.h2`
   font-family: brandon-grotesque;
@@ -43,7 +65,7 @@ const PageTitle = styled.h2`
   font-size: 24px;
   line-height: 30px;
   color: #2d2d2d;
-  margin-top: 16px;
+  padding-top: 16px;
   margin-bottom: 8px;
 `
 
@@ -51,6 +73,8 @@ const InsuranceCard = styled.div`
   width: 343px;
   height: 164px;
   border: 1px solid #e6e6e6;
+  background-color: #ffffff;
+  border-radius: 4px;
 `
 const InsurancePolicy = styled.div`
   margin-left: 16px;
@@ -73,5 +97,28 @@ const Description = styled.p`
   font-size: 14px;
   line-height: 21px;
   margin-bottom: 8px;
+  color: #73777c;
+`
+
+const Divider = styled.div`
+  border-bottom: 1px solid #e0e4e8;
+`
+
+const CoverageDates = styled.h4`
+  margin-top: 8px;
+  font-family: brandon-grotesque;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 16px;
+  line-height: 24px;
+  color: #2d2d2d;
+`
+const CoverageDatesTitle = styled.h4`
+  margin-top: 4px;
+  font-family: bitter;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 18px;
   color: #73777c;
 `
